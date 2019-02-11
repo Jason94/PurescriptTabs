47912b2c0c169cee120a157b165d3ff046395109
Jason Walker <jason.walker@wicourts.gov>
2019-02-11 14:40:00 -0600

    Got PlainHTML working

--- src/Component.purs
+++ src/Component.purs
@@ -20,20 +20,27 @@ style styles = HH.attr (HH.AttrName "style") (joinWith ";" styles)
 --- Component
 ---
 
-type TabData = forall a b.
+type TabDatum =
   { title :: String
-  , html :: HH.HTML a b
+  , html :: HH.PlainHTML
+  }
+type TabData = NonEmptyArray TabDatum
+
+tabTitles :: TabData -> NonEmptyArray String
+tabTitles arr = map (\x -> x.title) arr
+
+data TabContainerQuery a = OpenTab TabDatum a
+
+type State =
+  { selectedTab :: Int
+  , tabDatum :: TabDatum
   }
 
-data TabContainerQuery a = OpenTab String a
+select :: TabDatum -> State
+select tabDatum = { selectedTab: 0, tabDatum: tabDatum }
 
-type State = { selectedTitle :: String }
-
-select :: String -> State -> State
-select title s = s { selectedTitle = title }
-
-tabContainer :: forall m. NonEmptyArray String -> H.Component HH.HTML TabContainerQuery Unit Void m
-tabContainer tabTitles =
+tabContainer :: forall m. TabData -> H.Component HH.HTML TabContainerQuery Unit Void m
+tabContainer tabData =
   H.component
     { initialState: const initialState
     , render
@@ -44,34 +51,35 @@ tabContainer tabTitles =
 
   initialState :: State
   initialState =
-    { selectedTitle: head tabTitles
+    { selectedTab: 0
+    , tabDatum: head tabData
     }
 
   stylingFor :: forall r i. State -> String -> HH.IProp r i
-  stylingFor state title = if state.selectedTitle == title
+  stylingFor state title = if state.tabDatum.title == title
                            then style [ "background-color: goldenrod" ]
                            else style [ "" ]
 
-  renderButton :: State -> String -> H.ComponentHTML TabContainerQuery
-  renderButton state title = HH.button
-    [ HE.onClick $ HE.input_ (OpenTab title)
-    , stylingFor state title
+  renderButton :: State -> TabDatum -> H.ComponentHTML TabContainerQuery
+  renderButton state tabDatum = HH.button
+    [ HE.onClick $ HE.input_ (OpenTab tabDatum)
+    , stylingFor state tabDatum.title
     ]
-    [ HH.text title ]
+    [ HH.text tabDatum.title ]
 
   renderButtons :: State -> H.ComponentHTML TabContainerQuery
-  renderButtons state = HH.div_ $ map (renderButton state) (toArray tabTitles)
+  renderButtons state = HH.div_ $ map (renderButton state) (toArray tabData)
 
   render :: State -> H.ComponentHTML TabContainerQuery
   render state =
     HH.div_
       [ HH.h1_ [ HH.text "Hello NY City" ]
       , renderButtons state
+      , HH.fromPlainHTML state.tabDatum.html
       ]
 
   eval :: TabContainerQuery ~> H.ComponentDSL State TabContainerQuery Void m
   eval = case _ of
-    OpenTab title next -> do
-      state <- H.get
-      _ <- H.modify_ $ select title
+    OpenTab tabDatum next -> do
+      _ <- H.put $ select tabDatum
       pure next
--- src/Main.purs
+++ src/Main.purs
@@ -2,12 +2,20 @@ module Main where
 
 import Prelude
 
+import Data.Array.NonEmpty (fromArray, fromNonEmpty, toArray)
+import Data.NonEmpty ((:|))
 import Effect (Effect)
 import Halogen.Aff as HA
+import Halogen.HTML as HH
 import Halogen.VDom.Driver (runUI)
 import TabContainer (tabContainer)
 
 main :: Effect Unit
 main = HA.runHalogenAff do
   body <- HA.awaitBody
-  runUI (tabContainer ["One", "Two", "Three"]) unit body
+  let tabs = fromNonEmpty (
+        { title: "One", html: HH.span_ [ HH.text "Component A" ] }  :|
+        [ { title: "Two", html: HH.span_ [ HH.text "Component B" ] }
+        , { title: "Three", html: HH.span_ [ HH.text "Component C" ] }
+        ])
+  runUI (tabContainer tabs) unit body
